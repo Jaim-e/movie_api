94f6583a0a4efbd773d41875b80c87d79e60c00a
Jaim-e <51797216+Jaim-e@users.noreply.github.com>
2021-08-08 15:10:58 +0200

    Update models.js
    Importing Bcrypt and Setting the actual Hashing of submitted passwords.

--- models.js
+++ models.js
@@ -1,5 +1,9 @@
-const mongoose = require("mongoose");
+// Importing Mongoose and Bcrypt
+const mongoose = require("mongoose"),
+      bcrypt = require("bcrypt");
 
+
+// Declaring the Movie Schema
 let movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
   Decription: {type: String, required: true},
@@ -16,22 +20,33 @@ let movieSchema = mongoose.Schema({
   Featured: Boolean
 });
 
+
+// Declaring the User Schema
 let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
   Password: {type: String, required: true},
   Email: {type: String, required: true},
   Birthday: Date,
-  FavoriteMovies: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Movie"
-  }]
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
 });
+// Doing the actual Hashing of submitted passwords 
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+// Comparing submitted Hashed passwords with those stored in myFlixDB [DO NOT USE ARROW FUNTIONS HERE!!!]
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
+
+// Declaring the Genre Schema
 let genreSchema = mongoose.Schema({
   Name: {type: String, required: true},
   Description:{type: String, required: true}
 });
 
+
+// Declaring the Director Schema
 let directorSchema = mongoose.Schema({
   Name: {type: String, required: true},
   Bio: {type: String, required: true},
@@ -40,12 +55,12 @@ let directorSchema = mongoose.Schema({
 });
 
 
-
 // Building models
 let Movie = mongoose.model("Movie", movieSchema),
     User = mongoose.model("User", userSchema),
     Genre = mongoose.model("Genre", genreSchema),
     Director = mongoose.model("Director", directorSchema);
+
 
 // Exporting models
 module.exports.Movie = Movie;
