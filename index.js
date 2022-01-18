b33bb3ac44018956c8acb7585904f1cdea0ba1f5
Jaim-e <jaimemiazza@gmail.com>
2022-01-18 18:15:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,18 +35,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("common"));
 
 // Allowing requests from a certain list of domains
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com", "http://localhost:1234", "https://www.accioncine.es", "https://www.imdb.com"];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      // If a specific origin isn't found on the list of allowed origins
-      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+//let allowedOrigins = ["http://localhost:8080", "http://testsite.com", "http://localhost:1234", "https://www.accioncine.es", "https://www.imdb.com"];
+app.use(cors());
 
 // Importing auth.js into the project and allow using Express
 let auth = require("./auth")(app);
